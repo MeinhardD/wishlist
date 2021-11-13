c69c5d1b02d0dd2b9ef799b1d76021ab01cbf8b2
Meinhard Dam <mei.dam@protonmail.com>
2021-11-13 16:58:36 +0000

    Added mimetype to image

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -2,20 +2,30 @@
 
 namespace App\Http\Controllers;
 
-use Exception;
-use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Storage;
 
 class ImageController extends Controller
 {
     public function show($image_name)
     {
-        try {
-            $image = Storage::get('/public/images/' . $image_name);
-        } catch (Exception $e) {
-            return abort(404);
+        $path = '/public/images/' . $image_name;
+
+        if (Storage::exists($path)) {
+            return $this->getImage($path);
         }
 
-        return $image;
+        return abort(404);
+    }
+
+    private function getImage(String $path)
+    {
+        $content = Storage::get($path);
+        $mime = Storage::mimeType($path);
+
+        $response = Response::make($content, 200);
+        $response->header('Content-Type', $mime);
+
+        return $response;
     }
 }
