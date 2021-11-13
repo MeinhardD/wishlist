7dea0ef5672f0c730be7b97279823a6c2d556220
Meinhard Dam <mei.dam@protonmail.com>
2021-11-13 12:58:32 +0000

    Added unlocking route

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,11 @@
 
 use App\Http\Controllers\Api\ItemController;
 use App\Http\Controllers\Api\WishlistController;
+use App\Models\Wishlist;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Hash;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,3 +30,38 @@ Route::delete('/wishlists/{unique_link}', [WishlistController::class, 'destroy']
 Route::post('/items/store', [ItemController::class, 'store']);
 Route::post('/items/update', [ItemController::class, 'update']);
 Route::delete('/items/{id}', [ItemController::class, 'destroy']);
+
+Route::post('/unlock', function (Request $request) {
+    try {
+        $request->validate([
+            'unique_link' => ['required', 'string'],
+            'password' => ['required', 'string'],
+        ]);
+    } catch (Exception $e) {
+        return response()->json([
+            'message' => 'Both the unique link and a password are required',
+            'success' => false,
+        ]);
+    }
+
+    try {
+        $wishlist = Wishlist::find(Crypt::decrypt($request->unique_link));
+    } catch (Exception $e) {
+        return response()->json([
+            'message' => 'Could not find the wishlist to unlock',
+            'success' => false,
+        ]);
+    }
+
+    if (Hash::check($request->password, $wishlist->password)) {
+        return response()->json([
+            'message' => 'Unlocked the wishlist',
+            'success' => true,
+        ]);
+    }
+
+    return response()->json([
+        'message' => 'Wrong password',
+        'success' => false,
+    ]);
+});
